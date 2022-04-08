2c26dca8ded83dd22cc207e6c877b1e2beeb7579
leroy-netizen <leroythomas051@gmail.com>
2022-04-08 18:09:20 +0300

    create Register form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
-
+import {useState, useEffect} from 'react'
+import {FaSignInAlt} from 'react-icons/fa'
 const Login = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        passwordConfirm: ''
+    })
+
+    const {name, email, password, password2 } = formData;
+    const onChange =(e) => {
+        setFormData((prevState)=> ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }))
+    }
+    const onSubmit = (e) => {
+        e.preventDefault()
+    }
+
+
   return (
-    <div>Login page</div>
+    <>
+    <section className='heading'>
+        <h1>
+            <FaSignInAlt /> LOgin
+        </h1>
+        <p> Login </p>
+
+        <form onSubmit={onSubmit}>
+            {/* <div className="form-group">
+                <input type="text" className="form-control" id='name' name='name' value={name} placeholder="Please enter your name" onChange={onChange}/>
+            </div> */}
+            <div className="form-group">
+                <input type="email" className="form-control" id='email' name='email' value={email} placeholder="Please enter your email" onChange={onChange}/>
+            </div>
+            <div className="form-group">
+                <input type="password" className="form-control" id='password' name='password' value={name} placeholder="Please enter your password" onChange={onChange}/>
+            </div>
+            {/* <div className="form-group">
+                <input type="passwordConfirm" className="form-control" id='passwordConfirm' name='passwordConfirm' value={name} placeholder="Confirm passord" onChange={onChange}/>
+
+            </div> */}
+            <div className="form-group">
+                <button type='submit' className='btn btn-block'>Submit</button>
+            </div>
+        </form>
+    </section>
+    </>
   )
 }
 
